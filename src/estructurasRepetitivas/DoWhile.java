86d95f716c7e5f41d0a45269c434211812893613
Ariana Lozano <al.lozanop1@uniandes.edu.co>
2025-02-03 22:33:12 -0500

    Ejemplo Do While

---
+++ src/estructurasRepetitivas/DoWhile.java
@@ -0,0 +1,16 @@
+package estructurasRepetitivas;
+
+public class DoWhile {
+
+	public static void main(String[] args) {
+		int contador = 0;
+		
+		do {
+			System.out.println("Estoy en la vuelta Nº "+ contador);
+			contador ++; // contador = contador +1;
+		}
+		while (contador <= 10);
+		
+	}
+
+}
